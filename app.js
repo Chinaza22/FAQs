2dfed1e60ff867267cb79288051c802f497c33a8
Chinaza Obidike <obidikechinaza01@gmail.com>
2022-06-07 19:41:41 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
-const buttons = document.querySelectorAll('.question-btn');
+const questions = document.querySelectorAll(".question");
 
-buttons.forEach(function(button){
+questions.forEach(function (question) {
+  const btn = question.querySelector(".question-btn");
+  // console.log(btn);
 
- button.addEventListener('click', function(e){
+  btn.addEventListener("click", function () {
+    // console.log(question);
 
-    const question =e.currentTarget.parentElement.parentElement;
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
 
-    question.classList.toggle("show-text")
-
- });
+    question.classList.toggle("show-text");
+  });
 });
+
+//FIRST ATTEMPT
+
+//const buttons = document.querySelectorAll('.question-btn');
+
+//buttons.forEach(function(button){
+
+ //button.addEventListener('click', function(e){
+
+   // const question =e.currentTarget.parentElement.parentElement;
+
+   // buttons.forEach(function(item){
+       //if(item!== question){
+          // item.classList.remove("show-text");
+      // }
+    //})
+    //question.classList.toggle//("show-text")
+
+ //});
+//});
